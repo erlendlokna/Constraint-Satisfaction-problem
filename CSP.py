cf3b45ccd3c17ddb659469febf6cb828c5157ba2
erlend lokna <3rlendlokna@gmail.com>
2023-09-30 22:31:11 +0200

    added code

---
+++ CSP.py
@@ -0,0 +1,360 @@
+# CSP Assignment
+# Original code by Håkon Måløy
+# Updated by Xavier Sánchez Díaz
+
+import copy
+from itertools import product as prod
+import time
+
+class CSP:
+    def __init__(self):
+        # self.variables is a list of the variable names in the CSP
+        self.variables = []
+
+        # self.domains is a dictionary of domains (lists)
+        self.domains = {}
+
+        # self.constraints[i][j] is a list of legal value pairs for
+        # the variable pair (i, j)
+        self.constraints = {}
+
+        self.solution = None #CSP solution after backtrack search
+        self.backtracks_called = 0 #number of backtrack() function calls
+
+    def add_variable(self, name: str, domain: list):
+        """Add a new variable to the CSP.
+
+        Parameters
+        ----------
+        name : str
+            The name of the variable to add
+        domain : list
+            A list of the legal values for the variable
+        """
+        self.variables.append(name)
+        self.domains[name] = list(domain)
+        self.constraints[name] = {}
+
+    def get_all_possible_pairs(self, a: list, b: list) -> list[tuple]:
+        """Get a list of all possible pairs (as tuples) of the values in
+        lists 'a' and 'b', where the first component comes from list
+        'a' and the second component comes from list 'b'.
+
+        Parameters
+        ----------
+        a : list
+            First list of values
+        b : list
+            Second list of values
+
+        Returns
+        -------
+        list[tuple]
+            List of tuples in the form (a, b)
+        """
+        return prod(a, b)
+
+    def get_all_arcs(self) -> list[tuple]:
+        """Get a list of all arcs/constraints that have been defined in
+        the CSP.
+
+        Returns
+        -------
+        list[tuple]
+            A list of tuples in the form (i, j), which represent a
+            constraint between variable `i` and `j`
+        """
+        return [(i, j) for i in self.constraints for j in self.constraints[i]]
+
+    def get_all_neighboring_arcs(self, var: str) -> list[tuple]:
+        """Get a list of all arcs/constraints going to/from variable 'var'.
+
+        Parameters
+        ----------
+        var : str
+            Name of the variable
+
+        Returns
+        -------
+        list[tuple]
+            A list of all arcs/constraints in which `var` is involved
+        """
+        return [(i, var) for i in self.constraints[var]]
+
+    def add_constraint_one_way(self, i: str, j: str,
+                               filter_function: callable):
+        """Add a new constraint between variables 'i' and 'j'. Legal
+        values are specified by supplying a function 'filter_function',
+        that should return True for legal value pairs, and False for
+        illegal value pairs.
+
+        NB! This method only adds the constraint one way, from i -> j.
+        You must ensure to call the function the other way around, in
+        order to add the constraint the from j -> i, as all constraints
+        are supposed to be two-way connections!
+
+        Parameters
+        ----------
+        i : str
+            Name of the first variable
+        j : str
+            Name of the second variable
+        filter_function : callable
+            A callable (function name) that needs to return a boolean.
+            This will filter value pairs which pass the condition and
+            keep away those that don't pass your filter.
+        """
+        if j not in self.constraints[i]:
+            # First, get a list of all possible pairs of values
+            # between variables i and j
+            self.constraints[i][j] = self.get_all_possible_pairs(
+                self.domains[i], self.domains[j])
+
+        # Next, filter this list of value pairs through the function
+        # 'filter_function', so that only the legal value pairs remain
+        self.constraints[i][j] = list(filter(lambda
+                                             value_pair:
+                                             filter_function(*value_pair),
+                                             self.constraints[i][j]))
+
+    def add_all_different_constraint(self, var_list: list):
+        """Add an Alldiff constraint between all of the variables in the
+        list provided.
+
+        Parameters
+        ----------
+        var_list : list
+            A list of variable names
+        """
+        for (i, j) in self.get_all_possible_pairs(var_list, var_list):
+            if i != j:
+                self.add_constraint_one_way(i, j, lambda x, y: x != y)
+
+    def backtracking_search(self):
+        """This functions starts the CSP solver and returns the found
+        solution.
+        """
+        # Make a so-called "deep copy" of the dictionary containing the
+        # domains of the CSP variables. The deep copy is required to
+        # ensure that any changes made to 'assignment' does not have any
+        # side effects elsewhere.
+        assignment = copy.deepcopy(self.domains)
+
+        # Run AC-3 on all constraints in the CSP, to weed out all of the
+        # values that are not arc-consistent to begin with
+        self.inference(assignment, self.get_all_arcs())
+
+        # Call backtrack with the partial assignment 'assignment'
+        self.solution = self.backtrack(assignment)
+
+    def backtrack(self, assignment):
+        """The function 'Backtrack' from the pseudocode in the
+        textbook.
+
+        The function is called recursively, with a partial assignment of
+        values 'assignment'. 'assignment' is a dictionary that contains
+        a list of all legal values for the variables that have *not* yet
+        been decided, and a list of only a single value for the
+        variables that *have* been decided.
+
+        When all of the variables in 'assignment' have lists of length
+        one, i.e. when all variables have been assigned a value, the
+        function should return 'assignment'. Otherwise, the search
+        should continue. When the function 'inference' is called to run
+        the AC-3 algorithm, the lists of legal values in 'assignment'
+        should get reduced as AC-3 discovers illegal values.
+
+        """
+        self.backtracks_called += 1 #
+
+        if all(len(values) == 1 for values in assignment.values()):
+            return assignment  # Return the completed assignment
+
+        var = self.select_unassigned_variable(assignment)
+
+        for value in assignment[var]:
+
+            new_assignment = copy.deepcopy(assignment)
+
+            new_assignment[var] = [value]
+
+            # Run inference to update the new_assignment based on constraints
+            if self.inference(new_assignment, self.get_all_neighboring_arcs(var)):
+                result = self.backtrack(new_assignment)
+
+                if result is not None:
+                    return result  
+
+        return None
+
+    def select_unassigned_variable(self, assignment):
+        """The function 'Select-Unassigned-Variable' from the pseudocode
+        in the textbook. Should return the name of one of the variables
+        in 'assignment' that have not yet been decided, i.e. whose list
+        of legal values has a length greater than one.
+        """
+        for var, values in assignment.items():
+            if len(values) > 1:
+                return var
+
+         # If all variables are assigned, return None
+        return None
+
+    def inference(self, assignment, queue):
+        """The function 'AC-3' from the pseudocode in the textbook.
+        'assignment' is the current partial assignment, that contains
+        the lists of legal values for each undecided variable. 'queue'
+        is the initial queue of arcs that should be visited.
+        """
+        while queue:
+            i, j = queue.pop(0) 
+
+            if self.revise(assignment, i, j):
+                if not assignment[i]:
+                    return False  
+
+                for k in self.get_all_neighboring_arcs(i):
+                    if k[1] != j: 
+                        queue.append(k)
+
+        return True 
+
+    def revise(self, assignment, i, j):
+        """The function 'Revise' from the pseudocode in the textbook.
+        'assignment' is the current partial assignment, that contains
+        the lists of legal values for each undecided variable. 'i' and
+        'j' specifies the arc that should be visited. If a value is
+        found in variable i's domain that doesn't satisfy the constraint
+        between i and j, the value should be deleted from i's list of
+        legal values in 'assignment'.
+        """
+        revised = False
+
+        for value_i in assignment[i][:]:
+            legal = False
+
+            for value_j in assignment[j]:
+                if (str(value_i), str(value_j)) in self.constraints[i][j]:
+                    legal = True
+                    break
+
+            if not legal:
+                assignment[i].remove(value_i)
+                revised = True
+
+        return revised
+    
+
+class Sudoku(CSP):
+    def __init__(self, filename: str):
+        """Instantiate a CSP representing the Sudoku board found in the text
+        file named 'filename' in the current directory.
+
+        Parameters
+        ----------
+        filename : str
+            Filename of the Sudoku board to solve
+        """
+        super().__init__()
+
+        self.filename = filename
+
+        board = list(map(lambda x: x.strip(), open(filename, 'r')))
+
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == '0':
+                    self.add_variable('%d-%d' % (row, col), list(map(str,
+                                                                    range(1, 10))))
+                else:
+                    self.add_variable('%d-%d' % (row, col), [board[row][col]])
+
+        for row in range(9):
+            self.add_all_different_constraint(['%d-%d' % (row, col)
+                                            for col in range(9)])
+        for col in range(9):
+            self.add_all_different_constraint(['%d-%d' % (row, col)
+                                            for row in range(9)])
+        for box_row in range(3):
+            for box_col in range(3):
+                cells = []
+                for row in range(box_row * 3, (box_row + 1) * 3):
+                    for col in range(box_col * 3, (box_col + 1) * 3):
+                        cells.append('%d-%d' % (row, col))
+                self.add_all_different_constraint(cells)
+
+    def solve(self): 
+        t0 = time.time()
+        self.backtracking_search()
+        t1 = time.time()
+        
+        print(f"\nsolution for {self.filename}\n")
+        self.print_solution()
+        print(f"backtracks called: {self.backtracks_called}")
+        print(f"compute time: {round(t1- t0,3)} seconds")
+
+
+
+    def print_solution(self):
+        """Convert the representation of a Sudoku solution as returned from
+        the method CSP.backtracking_search(), into a human readable
+        representation.
+        """
+        assert self.solution,  'no solutions calculated. Run backtrach_search()'
+
+        for row in range(9):
+            for col in range(9):
+                print(self.solution['%d-%d' % (row, col)][0], end=" "),
+                if col == 2 or col == 5:
+                    print('|', end=" "),
+            print("")
+            if row == 2 or row == 5:
+                print('------+-------+------')
+
+
+class MapCSP(CSP):
+    def __init__(self):
+        """
+        Instantiate a CSP representing the map coloring problem from the
+        textbook. This can be useful for testing your CSP solver as you
+        develop your code.
+        """
+        super().__init__()
+                
+        states = ['WA', 'NT', 'Q', 'NSW', 'V', 'SA', 'T']
+        edges = {'SA': ['WA', 'NT', 'Q', 'NSW', 'V'],
+                'NT': ['WA', 'Q'], 'NSW': ['Q', 'V']}
+        colors = ['red', 'green', 'blue']
+        for state in states:
+            self.add_variable(state, colors)
+        for state, other_states in edges.items():
+            for other_state in other_states:
+                self.add_constraint_one_way(state, other_state, lambda i, j: i != j)
+                self.add_constraint_one_way(other_state, state, lambda i, j: i != j)
+
+    def solve(self):
+        t0 = time.time()
+        self.backtracking_search()
+        t1 = time.time()
+        
+        print(f"solution: \n {self.solution}")
+        print(f"backtracks called: {self.backtracks_called}\n")
+        print(f"compute time {round(t1-t0, 3)} seconds\n")
+
+if __name__ == "__main__":
+    #Easy sudoku
+    Sudoku("sudoku/easy.txt").solve()
+    
+    #medium sudoku
+    Sudoku("sudoku/medium.txt").solve()
+
+    #hard sudoku
+    Sudoku("sudoku/hard.txt").solve()
+
+    #very hard sudoku
+    Sudoku("sudoku/veryhard.txt").solve()
+
+    #Map problem
+    MapCSP().solve()
+
+
